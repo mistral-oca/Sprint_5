e66b4081900d7b40f402dd4132320c631c6b79c1
mrsMistral <obalzak@list.ru>
2025-11-23 23:02:15 +0100

    Вынесла вспомогательные модули в новый файл helpers.py. Относится к замечанию 5.

--- tests/test_ad_placement_by_authorized.py
+++ tests/test_ad_placement_by_authorized.py
@@ -1,9 +1,8 @@
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import *
-from selenium.common.exceptions import TimeoutException
+from helpers import get_next_ad_number, wait_for_last_ad_card
 
 EXISTING_USER_EMAIL = "ponomarevamaria_28@gmail.com"
 EXISTING_USER_PASSWORD = "1"
@@ -15,35 +14,9 @@ AD_CITY = "Москва"
 AD_CONDITION = "Б/У"
 
 
-def get_next_ad_number(driver):
-    all_ads = driver.find_elements(By.XPATH, "//div[contains(@class,'adCard')]//h2")
-    numbers = []
-    for ad in all_ads:
-        title = ad.text
-        if title.startswith("Тестовое объявление"):
-            parts = title.split()
-            if parts[-1].isdigit():
-                numbers.append(int(parts[-1]))
-    return max(numbers, default=0) + 1
+class TestCreateAd:
 
-
-def wait_for_last_ad_card(driver, ad_title, timeout=20):
-    try:
-        return WebDriverWait(driver, timeout).until(
-            lambda d: next(
-                (ad for ad in d.find_elements(By.XPATH, "//div[contains(@class,'card')]//h2")
-                 if ad_title in ad.text),
-                None
-            )
-        )
-    except TimeoutException:
-        return None
-
-
-
-class TestCreateAd:  
-
-    def test_create_ad_authorized(self, driver):  # <-- перенесли внутрь класса
+    def test_create_ad_authorized(self, driver):
         driver.get("https://qa-desk.stand.praktikum-services.ru/")
 
         driver.find_element(*LOGIN_REG_BUTTON).click()
