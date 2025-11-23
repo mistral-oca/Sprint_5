b6513ad9ded9f244d7eaa5fa2556e4e4bb4a49f8
mrsMistral <obalzak@list.ru>
2025-11-23 22:16:55 +0100

    Создала класс с проверками создания объявления. Относится к замечанию 1.

--- tests/ad_placement_by_unauthorized.py
+++ tests/ad_placement_by_unauthorized.py
@@ -4,13 +4,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import *
 
-def test_create_ad_unauthorized(driver):
-    driver.get("https://qa-desk.stand.praktikum-services.ru/")
 
-    driver.find_element(*PLACE_AD_BUTTON).click()
+class TestCreateAd:  
 
-    modal_title = WebDriverWait(driver, 5).until(
-        EC.visibility_of_element_located(MODAL_TITLE)
-    )
+    def test_create_ad_unauthorized(self, driver):
+        driver.get("https://qa-desk.stand.praktikum-services.ru/")
 
-    assert modal_title.is_displayed(), "Модальное окно не появилось"
+        driver.find_element(*PLACE_AD_BUTTON).click()
+
+        modal_title = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(MODAL_TITLE)
+        )
+
+        assert modal_title.is_displayed(), "Модальное окно не появилось"
